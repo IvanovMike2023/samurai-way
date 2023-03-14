ceb32dacd473cf8bc5dc97758d0cf1d20deb73b1
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-14 23:25:11 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./Component/Accordion";
 
 function App() {
   return (
     <div className="App">
+
      Hello, samurai! Let's go!
+        <Accordion text={'Летим!!!'} />
     </div>
   );
 }
