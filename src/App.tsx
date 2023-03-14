c0d4df256bbb401b6012e384ecffeef6827d1247
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-14 23:37:17 +0300

    resr

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Accordion from "./Component/Accordion";
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     Hello, samurai! Let's go!cascs
         <Accordion text={'Летим!!!'} />
     </div>
   );
