6aa75cbe46b38fb5efba35ce06722d9a6900059b
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-01-30 21:51:09 +0300

    lesson 56++

--- src/Component/Users/UsersContainer.tsx
+++ src/Component/Users/UsersContainer.tsx
@@ -1,17 +1,37 @@
 import react, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../Redux/store";
-import {SetTotalCountAC, SetUsersAC, UsersType} from "../../Redux/users-reducer";
+import {SetTotalCountAC, SetUsersAC, UsersArray, UsersType} from "../../Redux/users-reducer";
 import {Users} from "./Users/Users";
 import axios from "axios";
 import {log} from "util";
 
 export const UsersContainer = () => {
     const {users, totalCount, pagesize, currentpage} = useSelector<AppRootStateType, UsersType>(state => state.users)
-    //const {totalCount} = useSelector<AppRootStateType, UsersType>(state => state.users)
-
+    const instance = axios.create({withCredentials: true, baseURL: 'https://social-network.samuraijs.com/api/1.0'})
+    //  const UsersAPI = {
+    //     getUsers() {
+    //         return instance.get<CreateTodoListType>(`/users`)
+    //     }
+    // }
+    type CreateTodoListType = {
+        totalCount: number
+        error: null
+        data: {
+            items: UsersArray[]
+        }
+    }
     const dispatch = useDispatch()
     if (users.length === 0) {
+// useEffect(()=>{
+//     const promise =  UsersAPI.getUsers().then((res)=>{
+//             dispatch(SetUsersAC(res.data.data.items))
+//             dispatch(SetTotalCountAC(totalCount))
+//             console.log(res.data.data.items)
+//         }
+//     ).catch(er=>console.log(er))
+// },[])
+
         axios.get('https://social-network.samuraijs.com/api/1.0/users?count=5').then(res => {
             dispatch(SetUsersAC(res.data.items))
             dispatch(SetTotalCountAC(totalCount))
